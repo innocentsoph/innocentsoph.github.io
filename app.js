e10699f5dde61180f2379c50a2074702dc28d946
innocentsoph <innocentsmoothie1@gmail.com>
2024-09-18 12:47:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 document.addEventListener ("DOMContentLoaded", () => {
-
+    
     const images = document.querySelectorAll("img");
+
     for(const image of images){
-        fetch("https://dog.ceo/api/breeds/image/random")
+        fetch("https://stlucia.ceo/api/views/image/random")
         .then(response => response.json())
-        .then(data => image.src = data.message)
-    }
+        .then(data => {
+            image.src = data.message
+            image.width = 100;
+            image.height = 100;
+        })
+}
 
-
-} )
+})
